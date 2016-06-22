147abff4e7d534035e58d595feaa0a99fd54f3e3
linsalrob <raedwards@gmail.com>
2016-06-21 19:10:07 -0700

    converting strdup to dupstr

---
+++ src/dupstr.h
@@ -0,0 +1,17 @@
+
+
+/*
+ * dupstr.h
+ *
+ * Author: Rob Edwards
+ */
+
+#ifndef DUPSTR_H_
+#define DUPSTR_H_
+
+
+/* this is a drop in replacement for strdup() */
+char *dupstr(const char *s)
+
+
+#endif /* DUPSTR_H_ */
