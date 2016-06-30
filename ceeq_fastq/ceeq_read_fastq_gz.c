dd856169ce4a65a02e8ab4c51f2e2327ea26c9ee
Robert Edwards <raedwards@gmail.com>
2016-06-30 16:54:37 +1000

    adding a library for reading gzipped files

---
+++ ceeq_fastq/ceeq_read_fastq_gz.c
@@ -0,0 +1,99 @@
+/*
+ * read_fastq.c
+ *
+ *
+ *  Author: Rob Edwards
+ *
+ *  Read a fastq file and return a struct containing the sequence and information
+ */
+
+
+#include "ceeq_fastq.h"
+#include <ceeq_dupstr.h>
+#include <zlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* read_fastq
+ *
+ * Read a fastq file and return the number of sequences read.
+ *
+ * By default we include the @ in the sequence identifier. 
+ * This is a O(n) operation to remove (where n=length of id)
+ * and so it is costly to do for every sequence. There is a pop
+ * method here that you can use to remove that sign if you don't
+ * want it.
+ *
+ * Requires a pointer to a string for the file name, and a
+ * pointer to the hash where we should store the data.
+ *
+ */
+
+int read_fastq_gz(char *filename, struct fastq *seqs[]) {
+
+	gzFile fp;
+
+	char line[MAXLINELEN];
+	int seqcounter=0;
+
+	if ((fp = gzopen(filename, "r")) == NULL) {
+		fprintf(stderr, "Can't open file %s\n", filename);
+		exit(1);
+	}
+
+	char *ignored; /* this is to suppress a compiler warning*/
+	while ((ignored = gzgets(fp, line, MAXLINELEN)) != NULL) {
+
+		/* define our fastq data element */
+		struct fastq *nfq;
+		nfq = (struct fastq *) malloc(sizeof(*nfq));
+		if (nfq == NULL) {
+			fprintf(stderr, "Can't allocate memory for pointer\n");
+			return 0;
+		}
+
+		chomp(line);
+		// pop(line);
+
+		/* store the whole line */
+		nfq->info = dupstr(line);
+
+		/* store the sequence ID */
+		line[strcspn(line, " ")] = '\0';
+		nfq->id = dupstr(line);
+
+		/* read the sequence and save it */
+		ignored = gzgets(fp, line, MAXLINELEN);
+		if (line == NULL) {
+			fprintf(stderr, "There was no sequence for %s", nfq->info);
+			exit(1);
+		}
+
+		chomp(line);
+		nfq->seq = dupstr(line);
+
+		/* read the next line  and ignore it */
+		ignored = gzgets(fp, line, MAXLINELEN);
+
+		/* read the quality scores and save them */
+		ignored = gzgets(fp, line, MAXLINELEN);
+		if (line == NULL) {
+			fprintf(stderr, "There were no quality scores for %s", nfq->info);
+			exit(1);
+		}
+		chomp(line);
+		nfq->qual = dupstr(line);
+
+		if (add(nfq, "id", seqs) != NULL)
+			seqcounter++;
+		else
+			fprintf(stderr, "We got an error adding %s\n", nfq->id);
+
+	}
+
+	gzclose(fp);
+
+	return seqcounter;
+}
+
---
+++ ceeq_fastq/test_gz.c
@@ -0,0 +1,86 @@
+
+#include <ceeq_fastq.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <string.h>
+
+/*
+ * A test suite for reading fastq files. This tests all of the methods that we have written 
+ *
+ */
+
+#define ESEQS 5 // the number of sequences we expect to find in the file!
+
+
+
+
+int main(int argc, char *argv[]) {
+	// this file has 5 sequences in it so it should be easy to read!
+	char *filename = "../example_data/small.fastq.gz";
+	struct fastq *seqs[HASHSIZE] = {NULL};
+	int nseqs = read_fastq_gz(filename, seqs);
+	int error = 0;
+	if (nseqs != ESEQS) {
+		fprintf(stderr, "There was an error reading %s. We retrieved %d sequences but should have found %d sequences\n", filename, nseqs, ESEQS);
+		error = 1;
+	}
+
+	/* now test the get sequence methods */
+	int nnseqs = number_of_sequences(seqs);
+	if (nnseqs != ESEQS) {
+		printf("There was an error checking the number of sequences. We found %d using number_of_sequences but it should be %d\n", nnseqs, ESEQS);
+		error++;
+	}
+
+	/* test getting the ids */
+	char *ids[nnseqs];
+	nnseqs = get_ids(ids, seqs);
+	printf("Got %d ids\n", nnseqs);
+	for (int i=0; i<nnseqs; i++)
+		printf("ID %d: %s\n", i, ids[i]);
+	
+
+	char *sq = get_sequence("@GFH7CG303FQFE8/1", seqs);
+	char *sqans = "AAATACGGGTAGATATACCGCCATGTCTCGCAAACAGCCTCATCATGCCGCGCCTGCCCACTTTTTTGCATCAGTATCCCGGCATTGCGCTGGAACTTTGTAGTAGCGATCGACAGGTCGATTTACTACGGGAAGATTTCGACTGCGTGGTGCGCACAGAGCCGCTACATGCGCCAGGGATACTGACGCGCCCGCTTGGCAAACTGAGAAGAGTGAACTGCGCCAGCCCACAATACCTGGCGCGCTTTGGGTAATCCAGAAAAACCTTGACGATCTCGCCTCACATGCGGTGGTGCATTATTCATTGACCCCGGGAGTTTCTTCACCGGGTTTTGCCTTTGAAACTCCCACGGTATGCAGTGGTAAAAAACCGGCGGAAATGCTAACGGTAAACAGTACGAGACTTGGCATACAGCCTG";
+	char *quans = "@@@DDDDDFFFIIIIIIIIIIIIIIIIIIIIHHHIIFDDHHHHHHHHHHHHHFFFFFFFFF666666FFFFFFFFFFFFFCCADDDFDDCDDDDDD???6388CFFCCCCCDFFFFFDFFFF??8638<FF?AA>DFFFAA<<<FFFFFAACCFF???CFDDDDBBBBBBA<<889;888<<===BBBBB=;;A688<<9...3<4434443<<AAAAA??A==<>8624/2642..028>=<9989<<;4/..6;;<:,,,--..<<==:9<<<A<4444<=<<733224000<><8::CGGDDDDB::99400/0444322576;622....266666<68899444:8A?\?\?=>>>:3//.------55--266...3777666646...6297...269766///3:<><>;;93";
+	if (sq == NULL) {
+		fprintf(stderr, "Bugger, no sequence for @GFH7CG303FQFE8/1\n");
+		error++;
+	}
+
+	if (strcmp(sq, sqans)) {
+		error++;
+		printf("The sequence we retrieved for @GFH7CG303FQFE8/1 appeared to be wrong:\n%s\n", sq);
+	}
+
+	sq = get_sequence("NOSEQ", seqs);
+	if (sq != NULL) {
+		fprintf(stderr, "Using ID NOSEQ we retrieved %s\n", sq);
+		error++;
+	}
+
+
+	char *qual = get_quality("@GFH7CG303FQFE8/1", seqs);
+	if (strcmp(qual, quans)) {
+		fprintf(stderr, "The quality scores for @GFH7CG303FQFE8/1 do not appear correct\n");
+		error++;
+	}
+
+	qual = get_quality("NOSEQ", seqs);
+	if (qual != NULL) {
+		fprintf(stderr, "Got %s as quality scores for NOSEQ but shouldn't find any!\n", qual);
+		error++;
+	}
+
+
+
+	
+	if (error) 
+		fprintf(stderr, "There were %d errors when running the tests\n", error);
+	else
+		fprintf(stderr, "All tests passed\n");
+	
+	exit(error);
+
+}
