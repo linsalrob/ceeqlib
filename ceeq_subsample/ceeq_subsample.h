0e8043279b0d54582ce962a9e54bee70b2defc01
Robert Edwards <raedwards@gmail.com>
2016-09-18 13:28:15 -0700

    better documentation

--- ceeq_subsample/ceeq_subsample.h
+++ ceeq_subsample/ceeq_subsample.h
@@ -6,21 +6,38 @@
  * subsample_n
  *
  * Randomly choose n char *'s from a list of char *.
- * The parameters are the total number of things in the list,
- * the list, and the number that we want to choose.
  *
- * Returns a pointer to an array of those IDs.
+ * Parameters:
+ *    int: the number of elements in the array
+ *    char *[]: the array of char *'s
+ *    int: the number you want to subsample
+ *
+ * Returns:
+ *    char **:  a pointer to an array of those IDs.
  *
  */
+
+
 char **subsample_n(int nseqs, char *arr[], int n);
 
 /* 
- * subsample a fastq file. 
+ * subsample a fastq file and print the sequences
  *
- * Retriece nseqs sequences (or all the sequences in the file if nseqs is too small)
+ * Retrieve nseqs sequences (or all the sequences in the file if nseqs is too small)
  * from fastqfile.
  *
  * Optionally, compressed can be set to true to use a gzipped file
+ *
+ * Parameters:
+ *    int: the number of sequences to subsample 
+ *    char * : the name of the fastq file to subsample
+ *    int: whether the file is compressed
+ *
+ * Output:
+ *    prints the sequences that were subsampled
+ *
+ * Returns:
+ *    int: the number of sequences subsampled. 
  *
  */
 
