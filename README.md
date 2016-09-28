2f9abff6332c72fd993474188ce6e946e754d6ba
Rob Edwards <raedwards@gmail.com>
2016-09-28 14:09:16 -0700

    updating doc

--- README.md
+++ README.md
@@ -8,10 +8,10 @@ ceeqlib is written in pure C using only the standard library to ensure portabili
 Our libraries are split into core functions and sequence related functions. ceeqlib is designed to be standalone and to compile with vanilla C installations.
 
 ##core libraries##
-* *ceeq_arr* has functions for manipulating arrays, including shuffling elements in an array and swapping elements in an array. [ceeq_arr/ceeq_arr.h](ceeq_arr.h)
-* *ceeq_str* has functions for manipulating strings, including duplicating, concatenating and removing new lines. [ceeq_str/ceeq_str.h](ceeq_str.h)
+* *ceeq_arr* has functions for manipulating arrays, including shuffling elements in an array and swapping elements in an array. [ceeq_arr.h](ceeq_arr/ceeq_arr.h)
+* *ceeq_str* has functions for manipulating strings, including duplicating, concatenating and removing new lines. [ceeq_str.h](ceeq_str/ceeq_str.h)
 
-* *ceeq_fastq* has functions for reading fastq files and returning hashes of DNA sequences. You can [ceeq_fastq](read more) about the sequence 
+* *ceeq_fastq* has functions for reading fastq files and returning hashes of DNA sequences. You can [read more](ceeq_fastq) about the sequence 
 format or read [ceeq_fastq.h](ceeq_fastq/ceeq_fastq.h)
 
 
--- ceeq_fastq_pair/ceeq_fastq_pair.h
+++ ceeq_fastq_pair/ceeq_fastq_pair.h
@@ -1,5 +1,18 @@
 /*
- * fastq_pair_stream.h
+ * ceeq_fastq_pair.h
+ *
+ * Check paired end files to see if the pairs match.
+ *
+ * We use a modified version of our fastq data structure specifically for
+ * checking paired end files. In this case, we have added a seen flag
+ * (essentially a boolean) that lets us know if we have seen this sequence
+ * before. We iterate through the first file, and store all the sequences,
+ * and then as we stream the second file we note which sequences we find.
+ * Sequences in the second file are written directly either to the appropriate
+ * pair file or to the singles file. If there is a pair that is also written
+ * out. Finally, we iterate through the sequences from the first file, 
+ * identifying all those we have not seen and write them out.
+ *
  *
  * Author: Rob Edwards
  */
@@ -10,7 +23,8 @@
 #define MAXLINELEN 1000
 #define HASHSIZE 10000
 
-/* our fastq data structure.
+/* 
+ * This is our modified struct that contains a seen flag. 
  *
  * next is a pointer to the next structure
  * seqid is the sequence ID upto the first whitespace
@@ -29,16 +43,27 @@ struct fastq_pair_stream {
 };
 
 
-/* the primary method that takes two file names and figures out what sequences
- * are pairs
+/* 
+ * The primary method that takes the names of two files and figures out 
+ * which sequences in those files are paired, and which sequences are 
+ * singletons.
+ *
+ * Returns the total number of sequences read.
  */
 
 int fastq_pair_stream(char *, char *); /* primary method */
 
-/* calculate the hash value of a string */
+/* 
+ * Our hash function to calculate the hash value of a string.
+ * Returns an int with that vaue modded on the HASHSIZE
+ */
 unsigned hash(char *); /* what is the hash value for the string */
 
-/* add a fastqpair stream to our hash */
+/* 
+ * Add a sequence to our hash. 
+ *
+ * This accepts a struct  and adds it to the hash. 
+ */
 struct fastq_pair_stream *addfqs(struct fastq_pair_stream *s);
 
 #endif /* CEEQ_FASTQ_PAIR_H_ */
