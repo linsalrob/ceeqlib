4c07cd82324fd9e0bc39651c4474fd1963af90c6
Rob Edwards <raedwards@gmail.com>
2016-09-28 12:49:12 -0700

    updating docs

--- ceeq_str/ceeq_str.h
+++ ceeq_str/ceeq_str.h
@@ -3,9 +3,9 @@
 /*
  * ceeq_str.h
  *
- * These are straightforward implementations of strdup that duplicates
- * a string, and strcat that concatentates a string. The library
- * implementations are giving a malloc error.
+ * These are straightforward implementations of str functions.
+ * Some of the standard library implementations are giving a 
+ * malloc error.
  *
  * Part of the ceeq_ library. These software are released as is.
  *
@@ -15,19 +15,37 @@
 #define CEEQ_STR_H_
 
 
-/* dupstr takes a pointer string and returns a pointer to a copy of that string */
+/* 
+ * Duplicate a string.
+ *
+ * dupstr takes a pointer string and returns a pointer to a copy of that string
+ */
 char *dupstr(const char *s);
 
-/* catstr takes two pointers to strings and returns a new pointer to the concatenation of the two strings */
+/*
+ * Concatenate two strings
+ *
+ * catstr takes two pointers to strings and returns a new pointer to the concatenation of the two strings 
+ */
 char *catstr(const char *s, const char *t);
 
-/* remove the new line from a line read from a file */
+/* 
+ * Remove a new line from the the end of a string
+ * 
+ * chomp (named after the PERL function) takes a string and removes the new line from the end
+ */
 char *chomp(char *);
 
-/* remove the first character from a pointer to a line */
+/*
+ * Remove the first character from a string
+ *
+ * remove the first character from a pointer to a string
+ */
 char *pop(char *);
 
-/* get the substring of the first word (ie. upto the first white space character) if there is one in the string */
+/*
+ * get the substring of the first word (ie. upto the first white space character) if there is one in the string 
+ */
 char *firstword(char *line);
 
 #endif /* CEEQ_STR_H_ */
